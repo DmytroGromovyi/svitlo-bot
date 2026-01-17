808764e6893516d3a416dd663c77c281f9eeff4e
DmytroGromovyi <ricofromjungles@gmail.com>
2026-01-17 11:44:01 +0200

    removed redundant files

--- run_cron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Cron job script for schedule checking
-# This runs the notifier every 10 minutes
-
-while true; do
-    python notifier.py
-    sleep 600  # Wait 10 minutes (600 seconds)
-done
